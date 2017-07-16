9ce602a54042876cf23bbdc5f5a1a224be6f0eb5
Tomasz Rogalski <tomek@madebyrogal.com>
2017-07-16 18:02:30 +0400

    ADD first blood

---
+++ src/container/ReduxForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ReduxForm extends Component {
+
+    render() {
+        return (
+            <div className="redux-form">
+
+            </div>
+        );
+    }
+}
+
+export default ReduxForm;
